3505647ec797fa66e127ea1501cb83be50e4a96f
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-06-23 16:46:01 -0700

    create a test API route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.json({ message: "testing the API to check" });
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
+//dependencies
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+// routes
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 //connecting to database using mongoose
@@ -18,3 +21,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+//routes
+app.use("/api/user", userRoutes);
