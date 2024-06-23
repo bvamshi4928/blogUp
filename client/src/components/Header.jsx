e5aeabfae4efe04afbe119b846ddab97d6d4cb21
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-06-22 20:01:30 -0700

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg">
-          Vamshi's{" "}
+          Vamshi
         </span>
         Blog
       </Link>
@@ -32,7 +32,7 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button gradientDuoTone="purpleToBlue" outline>Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
