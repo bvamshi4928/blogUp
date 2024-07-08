154a4c9e11168a1d6559ba6c3ce5584adf481717
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-07-08 10:51:33 -0700

    create Signin API route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup, signin } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 //signing up a user
 router.post("/signup", signup);
 
-//signing in a user 
+//signing in a user
+router.post("/signin", signin);
 
 export default router;
