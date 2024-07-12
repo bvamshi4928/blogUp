caf2d7700d5735ab123cffa5329df94c20441731
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-07-12 10:07:47 -0700

    complete sidebar of dashboard

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 
-import(useSelector);
 
 const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />
 };
 
 export default PrivateRoute;
