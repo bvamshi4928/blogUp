c99af55bad8ac7dfb6df6e3bc2911bcf041de525
Bisapogu Vamshi <bvamshi4928@gmail.com>
2024-07-19 19:04:09 -0700

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { create ,getPosts,deletepost} from "../controllers/post.controller.js";
+import { create ,getPosts,deletepost,updatepost} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
@@ -13,4 +13,7 @@ router.get("/getposts", getPosts);
 //delete a post
 router.delete("/deletepost/:postId/:userId",verifyToken,deletepost);
 
+//update a post
+
+router.put("/updatepost/:postId/:userId",verifyToken,updatepost);
 export default router;
